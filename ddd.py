f85707fa3b52bee65a4f9d36f699f0b4b6f49163
Liu Yiling <lyleaf@gmail.com>
2015-11-18 23:36:24 +0100

    Update ddd.py

--- ddd.py
+++ ddd.py
@@ -2,40 +2,49 @@
 """
 Created on Wed Nov 18 14:15:48 2015
 
+Dijkstra Algorithm using Priority Queue
+
+Used PriorityQueue. Other choices: use heapq
+Didn't implement the heap that support deletion
+Used tuple in PriorityQueue. Other choices: use Class
+
+TODO: An updated version of Dijkstra. 
+
 @author: yiling
 """
-
 import Queue 
+
 with open("dijkstraData.txt","r") as f:
     lines = f.readlines()
 graph = [[tuple([int(i.split(",")[1]),int(i.split(",")[0])]) for i in line.split()[1:]] for line in lines]
 q = Queue.PriorityQueue()
 
-#class Vertex(object):
-#    def __init__(self, weight)
-distance = [100000 for i]
 
-A = [(0,1)]
-B = [range(2,201)]
-for i in graph[0]:
+distance = [100000 for i in xrange(len(graph))]
+
+s = 1 #start point
+
+
+A = [(0,s)]
+distance[s-1] = 0
+
+for i in graph[s-1]:
     q.put(i)
+    distance[i[1]-1] = i[0]
 #initialization part
-while True:
-    if q.empty(): break
-    x=q.get()
-    print x
-    if x[1] in map(lambda x:x[1],A):
+
+
+while (not q.empty()):
+    x = q.get()
+    if x[1] in map(lambda x:x[1],A):#This added a lot of complexity to my algorithm
         continue
+    A.append(x)
     for i in graph[x[1]-1]:
-        q.put(i[0]+x[0],i[1])
-    
-   
-q.get()
+        if i[0]+x[0] < distance[i[1]-1]:
+            distance[i[1]-1] = i[0]+x[0] 
+            q.put((distance[i[1]-1],i[1]))
 
-A.append()
+index = [7,37,59,82,99,115,133,165,188,197]
+ans = [distance[i-1] for i in index]
 
-while True:
-    x 
-    
-for weight,vertex in q.get():
-    pass
+print ans
